90cfcecbf350b51a74cc8993ddc7eb8e01e36806
Lucas Lacerda <lucaslacerdacl@gmail.com>
2019-05-23 23:24:08 -0300

    Fix lint

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { 
-    Container,
-    Card,
-    CardContent,
-    CardHeader,
-    Title,
-    Description,
-    CardFooter,
-    Annotation 
+import {
+	Container,
+	Card,
+	CardContent,
+	CardHeader,
+	Title,
+	Description,
+	CardFooter,
+	Annotation
 } from './style';
 import Menu from '../Menu';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -15,44 +15,38 @@ import { PanGestureHandler } from 'react-native-gesture-handler';
 
 export default class Content extends Component {
 
-    render() {
-        return (
-            <Container>
-                <Menu translateY={this.props.translateY} />
-                <PanGestureHandler 
-                    onGestureEvent={this.props.animatedEvent}
-                    onHandlerStateChange={this.props.onChange}>
-                    <Card style={{
-                        transform: [{
-                            translateY: this.props.translateY.interpolate({
-                                inputRange: [0, 460],
-                                outputRange: [0, 460],
-                                extrapolate: 'clamp'
-                            })
-                        }]
-                    }}>
-                        <CardHeader>
-                            <Icon name="attach-money" size={28} color="#666" />
-                            <Icon name="visibility-off" size={28} color="#999" />
-                        </CardHeader>
-                        <CardContent>
-                            <Title>
-                                Saldo disponível
-                            </Title>
-                            <Description>
-                                R$ 5.000,00
-                            </Description>
-                        </CardContent>
-                        <CardFooter>
-                            <Icon name="account-balance" size={28} color="#666" />
-                            <Annotation>
-                                Transferência de R$ 20,00 recebida de Diego Schell hoje às 06:00h
-                            </Annotation>
-                            <Icon name="chevron-right" size={28} color="#999" />
-                        </CardFooter>
-                    </Card>
-                </PanGestureHandler>
-            </Container>
-        )
-    }
+	render() {
+		return (
+			<Container>
+				<Menu translateY={this.props.translateY} />
+				<PanGestureHandler
+					onGestureEvent={this.props.animatedEvent}
+					onHandlerStateChange={this.props.onChange}>
+					<Card style={{
+						transform: [{
+							translateY: this.props.translateY.interpolate({
+								inputRange: [0, 460],
+								outputRange: [0, 460],
+								extrapolate: 'clamp'
+							})
+						}]
+					}}>
+						<CardHeader>
+							<Icon name="attach-money" size={28} color="#666" />
+							<Icon name="visibility-off" size={28} color="#999" />
+						</CardHeader>
+						<CardContent>
+							<Title>Saldo disponível</Title>
+							<Description>R$ 5.000,00</Description>
+						</CardContent>
+						<CardFooter>
+							<Icon name="account-balance" size={28} color="#666" />
+							<Annotation>Transferência de R$ 20,00 recebida de Diego Schell hoje às 06:00h</Annotation>
+							<Icon name="chevron-right" size={28} color="#999" />
+						</CardFooter>
+					</Card>
+				</PanGestureHandler>
+			</Container>
+		)
+	}
 }
--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, TabsContainer, TabItem, TabText } from './style.js';
 
-export default function Tabs ({ translateY }) {
-    return (
-        <Container style={{
-            opacity: translateY.interpolate({
-                inputRange: [0, 380],
-                outputRange: [1, 0.3],
-                extrapolate: 'clamp'
-            }),
-            transform: [{
-                translateY: translateY.interpolate({
-                    inputRange: [0, 380],
-                    outputRange: [0, 50],
-                    extrapolate: 'clamp'
-                })
-            }]
-        }}>
-            <TabsContainer>
-                <TabItem>
-                    <Icon name="person-add" size={24} color="#fff" />
-                    <TabText>Indicar amigos</TabText>
-                </TabItem>
-                <TabItem>
-                    <Icon name="chat-bubble-outline" size={24} color="#fff" />
-                    <TabText>Cobrar</TabText>
-                </TabItem>
-                <TabItem>
-                    <Icon name="arrow-downward" size={24} color="#fff" />
-                    <TabText>Depositar</TabText>
-                </TabItem>
-                <TabItem>
-                    <Icon name="arrow-upward" size={24} color="#fff" />
-                    <TabText>Transferir</TabText>
-                </TabItem>
-                <TabItem>
-                    <Icon name="lock" size={24} color="#fff" />
-                    <TabText>Bloquear cartão</TabText>
-                </TabItem>
-            </ TabsContainer>
-        </Container>
-    );
+export default class Tabs extends Component {
+	render() {
+		return (
+			<Container style={{
+				opacity: this.props.translateY.interpolate({
+					inputRange: [0, 380],
+					outputRange: [1, 0.3],
+					extrapolate: 'clamp'
+				}),
+				transform: [{
+					translateY: this.props.translateY.interpolate({
+						inputRange: [0, 380],
+						outputRange: [0, 50],
+						extrapolate: 'clamp'
+					})
+				}]
+			}}>
+				<TabsContainer>
+					<TabItem>
+						<Icon name="person-add" size={24} color="#fff" />
+						<TabText>Indicar amigos</TabText>
+					</TabItem>
+					<TabItem>
+						<Icon name="chat-bubble-outline" size={24} color="#fff" />
+						<TabText>Cobrar</TabText>
+					</TabItem>
+					<TabItem>
+						<Icon name="arrow-downward" size={24} color="#fff" />
+						<TabText>Depositar</TabText>
+					</TabItem>
+					<TabItem>
+						<Icon name="arrow-upward" size={24} color="#fff" />
+						<TabText>Transferir</TabText>
+					</TabItem>
+					<TabItem>
+						<Icon name="lock" size={24} color="#fff" />
+						<TabText>Bloquear cartão</TabText>
+					</TabItem>
+				</ TabsContainer>
+			</Container>
+		);
+	}
 }
